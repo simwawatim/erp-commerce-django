f45a35ea26c10c7ccdfa626ffa84e96e1e705815
simwawatim <simwawatimothy64@gmail.com>
2025-07-04 08:58:15 +0200

    added shipping address field to sals models

--- base/models.py
+++ base/models.py
@@ -110,6 +110,7 @@ class SalesOrder(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     date_ordered = models.DateTimeField(auto_now_add=True)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True, blank=True)
+    shipping_address = models.TextField(blank=True, null=True)
 
 
 
